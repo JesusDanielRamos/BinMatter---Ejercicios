2a654951f462caf1845e37c1deae74a63d6b6e02
JesusDanielRamos <03ramosjesus@gmail.com>
2025-08-28 13:31:23 -0600

    version 2, actualizada

---
+++ Proyecto HTML/result.js
@@ -0,0 +1,16 @@
+
+
+let nuevoValor = 35;
+document.getElementById("resultPercentage").textContent = nuevoValor;
+
+
+let score1 = 89;
+let score2 = 92;
+let score3 = 61;
+let score4 = 72;
+let topScore = 100;
+document.getElementById("top-score").textContent = topScore;
+document.getElementById("score-1").innerHTML = `<b>${score1}</b>`;
+document.getElementById("score-2").innerHTML = `<b>${score2}</b>`;
+document.getElementById("score-3").innerHTML = `<b>${score3}</b>`;
+document.getElementById("score-4").innerHTML = `<b>${score4}</b>`;
